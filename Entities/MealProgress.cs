4251661c7b9e6992f20e8a7e80f27abeb3c26e3a
kerem kartal <keremkartal491741@gmail.com>
2025-03-24 12:34:57 +0300

    diyet listesi görüntüleme tamam

---
+++ Entities/MealProgress.cs
@@ -0,0 +1,22 @@
+﻿namespace DietApp.Entities
+{
+    public class MealProgress
+    {
+        public int Id { get; set; }
+
+        // Hangi diet listesine ait (ForeignKey)
+        public int DietListId { get; set; }
+
+        // Gün ismi örn: "Pazartesi"
+        public string DayName { get; set; }
+
+        // Dictionary üzerinde kullandığımız index veya meal unique kimliği
+        public int MealIndex { get; set; }
+
+        // Öğün tamamlandı mı?
+        public bool IsCompleted { get; set; }
+
+        // Ne zaman tamamlandı (opsiyonel)
+        public DateTime? CompletedAt { get; set; }
+    }
+}
